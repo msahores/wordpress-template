e19c3168658caf6a7038f6ceafb897ab8d5df7c5
Manuel Sahores <manuel.sahores@rga.com>
2019-07-08 14:52:09 -0300

    Create a specific page template (following the name)

---
+++ page-about-me.php
@@ -0,0 +1,15 @@
+<?php get_header(); ?>
+<?php
+if(have_posts(  )): 
+    while(have_posts(  )):
+        the_post(  ); ?>
+        <small>Posted on <?php the_time('F j, Y'); ?> at <?php the_time( 'g:i a' )?>, in <?php the_category();?></small>
+        <p><?php the_content( )?></p>
+        <h3><?php the_title();?></h3>
+        <hr>
+    <?php
+    endwhile;
+    endif;            
+
+?>
+<?php get_footer(); ?>
