5c8263be36e254e98f18d7c507b30fc1118553bc
Manuel Sahores <manuel.sahores@rga.com>
2019-07-08 14:21:37 -0300

    Custom classes

--- header.php
+++ header.php
@@ -5,7 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Awesome Template</title>
-    <?php wp_head() ?>
+    <?php wp_head(); ?>
 </head>
-<body>
-    <?php wp_nav_menu(array('theme_location' => 'primary') )?>
+    <?php 
+    if(is_front_page() ): //is_home() is used for blog page
+        $awesome_classes = array('awesome_class', 'my_class');
+    else:
+        $awesome_classes = array('not_awesome_class');
+    endif;
+    ?>
+<body <?php body_class($awesome_classes ); ?>>
+    <?php wp_nav_menu(array('theme_location' => 'primary') ); ?>
