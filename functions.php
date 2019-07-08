ab134ccd644b6d6586d7093b0bf5bcc922f9215f
Manuel Sahores <manuel.sahores@rga.com>
2019-07-08 16:42:06 -0300

    Showing different post formats

--- functions.php
+++ functions.php
@@ -17,4 +17,5 @@
     add_theme_support( 'custom-background' );
     add_theme_support( 'custom-header' );
     add_theme_support( 'post-thumbnails' );
+    add_theme_support( 'post-formats', array('aside', 'image', 'video') );
 ?>
