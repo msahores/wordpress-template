dd3f18119ddb9dfbfd1e66c3d036975687af7c8d
Manuel Sahores <manuel.sahores@rga.com>
2019-07-10 17:12:18 -0300

    Post classes

--- content.php
+++ content.php
@@ -1,6 +1,7 @@
-<h3><?php the_title();?></h3>
-<p>Post Format: <?php echo get_post_format() ?></p>
-<div class="thumbnail img"><?php the_post_thumbnail('thumbnail') ?></div> <?php  // options: 'large'?>
-<small>Posted on <?php the_time('F j, Y'); ?> at <?php the_time( 'g:i a' )?>, in <?php the_category();?></small>
-<p><?php the_content( )?></p>
-<hr>
+<article id="post-<?php the_ID();?>" <?php post_class(); ?>>
+    <h3><?php the_title();?></h3>
+    <p>Post Format: <?php echo get_post_format() ?></p>
+    <div class="thumbnail img"><?php the_post_thumbnail('thumbnail') ?></div> <?php  // options: 'large'?>
+    <small>Posted on <?php the_time('F j, Y'); ?> at <?php the_time( 'g:i a' )?>, in <?php the_category();?></small>
+    <p><?php the_content( )?></p>
+</article>
