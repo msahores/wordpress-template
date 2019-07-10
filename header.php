3cfc7fce8eb6c8213cc73809513e049d6becf069
Manuel Sahores <manuel.sahores@rga.com>
2019-07-10 17:58:09 -0300

    Posts title with link

--- header.php
+++ header.php
@@ -18,7 +18,7 @@
         
 <div class="container">
     <div class="row">
-     <div class="col-xs-12">
+     <div class="col-xs-12 col-sm-12">
      <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <a class="navbar-brand" href="#">Awesome</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
