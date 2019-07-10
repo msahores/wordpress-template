d685d7aadaa1fbaf94738f4406af9f1f90ad8d7f
Manuel Sahores <manuel.sahores@rga.com>
2019-07-10 17:59:20 -0300

    Posts title with link

--- content.php
+++ content.php
@@ -1,7 +1,9 @@
 <article id="post-<?php the_ID();?>" <?php post_class(); ?>>
-    <h3><?php the_title();?></h3>
-    <p>Post Format: <?php echo get_post_format() ?></p>
-    <div class="thumbnail img"><?php the_post_thumbnail('thumbnail') ?></div> <?php  // options: 'large'?>
+<header class="entry-header">
+    <?php the_title( sprintf('<h1 class="entry-title"><a href="%s">', esc_url(get_permalink())),'</a></h1>');?>
     <small>Posted on <?php the_time('F j, Y'); ?> at <?php the_time( 'g:i a' )?>, in <?php the_category();?></small>
-    <p><?php the_content( )?></p>
+</header>    
+<p>Post Format: <?php echo get_post_format() ?></p>
+<div class="thumbnail img"><?php the_post_thumbnail('thumbnail') ?></div> <?php  // options: 'large'?>
+<p><?php the_content( )?></p>
 </article>
